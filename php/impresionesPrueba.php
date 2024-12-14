fcf4dbcf115b134d874f9963e04dc3c9ad1a838c
Asaph Velazquez <saulvelpa120@gmail.com>
2024-12-13 18:00:03 -0600

    actualiacion

--- php/impresionesPrueba.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Conexión a la base de datos
-$conexion = mysqli_connect('localhost', 'root', '', 'casilleros');
-$select = "SELECT * FROM estudiantes";
-$resultado = mysqli_query($conexion, $select);
-$valida = mysqli_num_rows($resultado);
-echo "En tu base de datos casilleros tienes $valida registros";
-
-?>
