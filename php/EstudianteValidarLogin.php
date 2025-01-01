efdd35395e1a10507994c849d291e2599599feb7
miguelarenas0809 <91294677+miguelarenas0809@users.noreply.github.com>
2025-01-01 13:41:34 -0600

    Update EstudianteValidarLogin.php

--- php/EstudianteValidarLogin.php
+++ php/EstudianteValidarLogin.php
@@ -27,8 +27,72 @@
         // Verificar la contraseña ingresada con la encriptada en la base de datos
         if (password_verify($contrasena, $row['contraseña'])) {
             // Redireccionar a la página principal 
-            header('Location: ../html/PagPrincipal.html');  //PROVISIONAL EN TEORIA DEBERIA REDIRECCIONAR A LA PAGINA DEL PDF
-            exit();
+            //header('Location: ../html/PagPrincipal.html');  //PROVISIONAL EN TEORIA DEBERIA REDIRECCIONAR A LA PAGINA DEL PDF
+
+            $consultaSolicitud = "SELECT S.* FROM solicitudes as S INNER JOIN estudiantes as E ON S.id_estudiante = E.id_estudiante WHERE E.usuario = '$usuario'";
+            $resultadoSolicitud = mysqli_query($conexion, $consultaSolicitud);
+            if (!$resultadoSolicitud) { // Verificar si la consulta tuvo éxito
+                die('Error en la consulta resultado de Solicitud: ' . mysqli_error($conexion));
+            }
+            $filaSolicitud = mysqli_fetch_assoc($resultadoSolicitud);
+            $estatusS = $filaSolicitud['estatus'];
+            
+
+            if($estatusS == 'Asignado'){ //Mostrar solo la opcion de imprimir acuse y cerrar sesión
+
+            }
+            else if($estatusS == 'Pendiente'){ //Casos para recibir comprobante de pago
+                 
+                //Comprobar si hay menos de 100 en la BD
+                $numSolicitudes = "SELECT COUNT(*) AS tot FROM solicitudes";
+                $resultadoNumS = mysqli_query($conexion, $numSolicitudes);
+                if(!$resultadoNumS){
+                    die('Error en la consulta numero de Solicitudes: ' . mysqli_error($conexion));
+                }
+                $filaNum = mysqli_fetch_assoc($resultadoNumS);
+
+                if($filaNum['tot'] < 100){ //Si hay menos de 100 solicitudes guardadas continuar permitiendo el acceso
+                
+
+                    $tipoSolicitud = $filaSolicitud['tipo_solicitud'];
+
+                    $fechaSolicitud = $filaSolicitud['fecha_registro'];
+                    $fechaS = new DateTime($fechaSolicitud);
+                    $fechaAct = new DateTime(); // Obtiene la fecha y hora actual
+                    $diferencia = $fechaAct->diff($fechaS);
+                    $horas = ($diferencia->days * 24) + $diferencia->h;
+
+                    if($tipoSolicitud == "Registro"){ //Pagina para resigistro por primera vez
+
+                        if($horas >= 48){ //Tiempo mayor que 48 hrs dejar acceder
+                            echo "Accediendo para registro por primera vez $tipoSolicitud $fechaSolicitud";
+                        }
+                        else{ //Esperar a que pasen 48 hrs
+                            echo "Espera a que pasen 48 horas de haber realizado tu registro";
+                        }
+                    }
+                    else{ //Solicitud de renovación
+    
+                        // Verificar si han pasado menos de 24 horas
+                        if ($horas < 24) {
+                            echo "Aun en tiempo, solicitud de renovacion";
+                        } 
+                        else {
+                            echo "Fuera de tiempo, en espera de reasignacion";
+                        }
+                    }
+                }
+                else{ //Mostrar mensaje de en espera
+                    echo "En espera, limite de solicitudes alcanzado";
+                }
+            }
+            else if($estatusS == 'Lista de espera'){ //En lista de espera
+
+            }
+            else{ //Agregar estado de solicitud rechazada
+
+            }
+            //exit();
         } else {
             // Contraseña incorrecta
             echo 'Contraseña incorrecta.';
